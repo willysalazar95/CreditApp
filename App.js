cbc076a841adc5c16db8e8774cdd1b19a328d5b6
unknown <alngc980@gmail.com>
2023-02-22 21:02:28 -0500

    Cambios AAGC

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+//AAGC 22/02/2023
 export default function App() {
   return (
     <View style={styles.container}>
