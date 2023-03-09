a15c2e896d65a6231319df2cc80cb6e7dab2d69e
kefer <morafi.1999.16@gmail.com>
2023-03-08 22:12:26 -0500

    se implementa clases

---
+++ src/clases/Usuario.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export class Usuario {
+	nIdUsers;
+	nIdPers;
+	cUsuario;
+	cClave;
+	bEstado;
+	nTipoUsuario;
+
+	async loginUser(username, password) {
+		const BASE_URL = "http://aagc.somee.com/api/Usuarios/";
+
+		try {
+			const response = await axios.get(BASE_URL + "Login", {
+				params: {
+					cUsuario: username,
+					cClave: password,
+				},
+			});
+
+			const Resp = response.data.code;
+			const Lista = response.data.data;
+
+			if (Resp === 200) {
+				return { success: true, data: Lista[0] };
+			} else {
+				return { success: false, error: "Datos incorrectos" };
+			}
+		} catch (error) {
+			return { success: false, error: error.message };
+		}
+	}
+}
