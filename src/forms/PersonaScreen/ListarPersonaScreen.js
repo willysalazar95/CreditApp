36fca61bbf3ecd380edd35329faaf87ebc530251
alngc980 <alngc980@gmail.com>
2023-03-11 16:20:26 -0500

    Creacion del buscar cliente

--- src/forms/PersonaScreen/ListarPersonaScreen.js
+++ src/forms/PersonaScreen/ListarPersonaScreen.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, FlatList, TouchableOpacity, Alert } from "react-native";
-import { useNavigation, useFocusEffect  } from "@react-navigation/native";
+import { View, StyleSheet, Text, TextInput, FlatList, TouchableOpacity, Alert } from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { Cliente } from "../../clases/Cliente";
+import { Ionicons } from 'react-native-vector-icons';
 
 const ListarPersonaScreen = () => {
   const [personas, setPersonas] = useState([]);
+  const [query, setQuery] = useState('');
   const [personaSeleccionada, setPersonaSeleccionada] = useState(null);
   const navigation = useNavigation();
 
   const ListarPersonas = async () => {
     const DatCliente = new Cliente();
-    const response = DatCliente.ListarPersonas();
-    // console.log((await response).data);
-    setPersonas((await response).data);
+    const response = await DatCliente.ListarPersonas();
+    setPersonas(response.data);
   }
+
+  const BuscarPersonas = async () => {
+    const DatCliente = new Cliente();
+    const response = await DatCliente.ListarPersonas();
+    const filteredData = response.data.filter((item) => {
+      return item.cPersNombres.toLowerCase().includes(query.toLowerCase())
+    });
+    setPersonas(filteredData);
+  }
+
   useEffect(() => {
     ListarPersonas();
   }, []);
@@ -23,14 +34,6 @@ const ListarPersonaScreen = () => {
       ListarPersonas();
     }, [])
   );
-
-  // useEffect(() => {
-  //   fetch("http://aagc.somee.com/api/personas/ListarPersonas")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setPersonas(data.data);
-  //     });
-  // }, []);
 
   const renderItem = ({ item }) => {
     const handleModificar = () => {
@@ -96,6 +99,30 @@ const ListarPersonaScreen = () => {
         data={personas}
         renderItem={renderItem}
         keyExtractor={(item) => item.nIdPers}
+        ListHeaderComponent={
+          // <View style={styles.searchBar}>
+          //   <TextInput
+          //     style={styles.searchInput}
+          //     placeholder="Buscar por nombre"
+          //     value={query}
+          //     onChangeText={(text) => BuscarPersonas(text)}
+          //   />
+          // </View>
+
+          <View style={styles.inputContainer}>
+            <Ionicons name="ios-search" size={24} color="#aaa" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Buscar"
+              value={query}
+              onChangeText={setQuery}
+            />
+            <TouchableOpacity style={styles.buttonSearch} onPress={BuscarPersonas}>
+              <Ionicons name="search-outline" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+
+        }
       />
 
       <TouchableOpacity style={styles.button} onPress={goToRegister}>
@@ -120,6 +147,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  inputContainer: {
+    backgroundColor: '#eee',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 10,
+    padding: 5
+  },
   button: {
     backgroundColor: '#007299',
     borderRadius: 30,
@@ -133,6 +172,18 @@ const styles = StyleSheet.create({
     bottom: 10,
     right: 10,
     zIndex: 1,
+  },
+  buttonSearch: {
+    backgroundColor: '#007299',
+    borderRadius: 5,
+    padding: 5,
+    marginLeft: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSearchText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
   text: {
     fontSize: 24,
