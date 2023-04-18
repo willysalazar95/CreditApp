50618fb4e9c44629c2119fbd2003beaaaa307f61
elvisdelaguila <elvisdelaguila@gmail.com>
2023-04-17 22:38:24 -0500

    Update SimularCredito_Screen.tsx

---
+++ src/forms/Creditos/SimularCredito_Screen.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+
+const SimularCredito_Screen = ()=>{
+
+
+
+    return(
+<View>
+
+    
+</View>
+
+    );
+}
+ 
+export default SimularCredito_Screen;
